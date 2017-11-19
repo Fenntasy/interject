8ff80709cfe666b7dae49b1b63e4e3364df1390f
Vincent Billey <vincent.billey@gmail.com>
2017-11-19 18:08:20 +0100

    manage a yaml option file

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Remark-serve
 
+## Usage
+
 Go into a directory with one or more remark `.md` files and launch `remark-serve`.
 
 Each slides will have a URL.
@@ -7,3 +9,36 @@ Each slides will have a URL.
 ## Installation
 
 `npm install -g remark-serve`
+
+## Options
+
+You can have a YML file at the root of the repository where you want to use `remark-serve` with the following options.
+
+`css_files`: (array) list of CSS files that will be includes in the final HTML
+
+`css`: (string) direct CSS that will be included
+
+`remarkOptions`: (string) JavaScript options passed to the `remark.create(options)` call.
+This must be a string to work.
+
+Example:
+
+```yaml
+css_files:
+  - https://fonts.googleapis.com/css?family=Open+Sans|Roboto:400,900
+  - https://fonts.googleapis.com/css?family=Source+Code+Pro
+css: >
+  blockquote {
+    color: green;
+  }
+remarkOptions: >
+  {
+    ratio: "16:9",
+    highlightLanguage: "javascript",
+    highlightStyle: "tomorrow",
+    navigation: {
+      scroll: false
+    },
+    slideNumberFormat: "%current%"
+  }
+```
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #! /usr/bin/env node
+const { exec } = require("child_process");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const util = require("util");
+const readYaml = util.promisify(require("read-yaml"));
+
+const execAsPromise = util.promisify(exec);
 
 const currentDirectory = process.argv[2] || ".";
 
@@ -22,6 +27,55 @@ const findSlides = path =>
     });
   });
 
+const defaultStylesheets = [
+  "https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz)",
+  "https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic",
+  "https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700,400italic"
+];
+
+const defaultCss = `
+body { font-family: 'Droid Serif'; }
+h1, h2, h3 {
+font-family: 'Yanone Kaffeesatz';
+font-weight: normal;
+}
+.remark-code, .remark-inline-code { font-family: 'Ubuntu Mono'; }
+img {
+display: inline-block;
+vertical-align: middle;
+max-width: 100%;
+height: auto; }
+`;
+
+const readOptions = () =>
+  execAsPromise("git rev-parse --show-toplevel")
+    .then(({ stdout: rootDir }) =>
+      readYaml(path.join(rootDir.trim(), ".remark-serve.yml"))
+    )
+    .then(
+      yamlOptions => {
+        const remarkOptions = yamlOptions.remarkOptions
+          ? yamlOptions.remarkOptions
+          : "{}";
+        const stylesheets = yamlOptions.css_files
+          ? yamlOptions.css_files
+          : defaultStylesheets;
+        const style = yamlOptions.css ? yamlOptions.css : defaultCss;
+        return [
+          remarkOptions,
+          stylesheets
+            .map(url => `<link rel="stylesheet" href="${url}">`)
+            .join("\n") + `<style>${style}</style>`
+        ];
+      },
+      () => [
+        "{}",
+        defaultStylesheets
+          .map(url => `<link rel="stylesheet" href="${url}">`)
+          .join("\n") + `<style>${defaultCss}</style>`
+      ]
+    );
+
 const app = express();
 
 // Applying most used directories for assets
@@ -35,10 +89,19 @@ app.get("/:slide", function(request, result) {
   if (fs.existsSync(slide)) {
     readFile(path.join(__dirname, "template.html"))
       .then(template =>
-        Promise.all([template, readFile(path.join(currentDirectory, slide))])
+        Promise.all([
+          template,
+          readFile(path.join(currentDirectory, slide)),
+          readOptions()
+        ])
       )
-      .then(([template, data]) =>
-        result.send(template.replace("{{slides}}", data))
+      .then(([template, data, [remarkOptions, styles]]) =>
+        result.send(
+          template
+            .replace("{{styles}}", styles)
+            .replace("{{remarkOptions}}", remarkOptions)
+            .replace("{{slides}}", data)
+        )
       )
       .catch(error => result.send(error));
   } else {
